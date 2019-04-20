bdbb2926ecba368d03a908b880a9ab2ee5cf5316
Joshua Kitchen <joshdkitchen@gmail.com>
2019-04-20 11:31:19 +0100

    added integration tests for topic/queue REST API interactions

---
+++ simple-jms-integration-tests/src/test/java/kitchen/josh/simplejms/integrationtests/BrokerDestinationApiIndependenceIntegraionTest.java
@@ -0,0 +1,80 @@
+package kitchen.josh.simplejms.integrationtests;
+
+import kitchen.josh.simplejms.broker.Broker;
+import kitchen.josh.simplejms.broker.ConsumerId;
+import kitchen.josh.simplejms.broker.MessageModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Broker.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+public class BrokerDestinationApiIndependenceIntegraionTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    /**
+     * Sending messages to a topic does not send messages to queue consumers.
+     */
+    @Test
+    public void queueConsumersDoNotReceiveTopicMessages() {
+        ConsumerId consumerId = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+        restTemplate.postForEntity("/topic/send", new MessageModel("hello world"), Void.class);
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * Queue consumers can never receive a message using the topic receive endpoint.
+     */
+    @Test
+    public void queueConsumersCannotReceiveUsingTopicEndpoint() {
+        ConsumerId consumerId = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+        restTemplate.postForEntity("/topic/send", new MessageModel("hello world"), Void.class);
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/topic/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * Sending messages to a queue does not send messages to topic consumers.
+     */
+    @Test
+    public void topicConsumersDoNotReceiveQueueMessages() {
+        ConsumerId consumerId = restTemplate.postForEntity("/topic/consumer", null, ConsumerId.class).getBody();
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/topic/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * Topic consumers can never receive a message using the queue receive endpoint.
+     */
+    @Test
+    public void topicConsumersCannotReceiveUsingQueueEndpoint() {
+        ConsumerId consumerId = restTemplate.postForEntity("/topic/consumer", null, ConsumerId.class).getBody();
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+        restTemplate.postForEntity("/topic/send", new MessageModel("hello world"), Void.class);
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+}
---
+++ simple-jms-integration-tests/src/test/java/kitchen/josh/simplejms/integrationtests/BrokerQueueApiIntegrationTest.java
@@ -0,0 +1,149 @@
+package kitchen.josh.simplejms.integrationtests;
+
+import kitchen.josh.simplejms.broker.Broker;
+import kitchen.josh.simplejms.broker.ConsumerId;
+import kitchen.josh.simplejms.broker.MessageModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+/**
+ * Broker's Queue Rest API functionality.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Broker.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+public class BrokerQueueApiIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    /**
+     * Calling the create queue consumer endpoint returns unique consumer IDs.
+     */
+    @Test
+    public void createQueueConsumer_returnsUniqueIds() {
+        Set<UUID> consumerIds = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            ResponseEntity<ConsumerId> response = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(response.getBody()).isNotNull();
+
+            ConsumerId consumerId = response.getBody();
+            assertThat(consumerIds).doesNotContain(consumerId.getId());
+            consumerIds.add(consumerId.getId());
+        }
+    }
+
+    /**
+     * Unknown consumer IDs receive empty messages.
+     */
+    @Test
+    public void receiveMessage_unknownConsumerId_returnsOkAndEmptyMessage() {
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + UUID.randomUUID(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * When no messages exist to receive, a consumer receives an empty message.
+     */
+    @Test
+    public void receiveMessage_noMessages_returnsOkAndEmptyMessage() {
+        ConsumerId consumerId = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * When a message is sent before a consumer is created, the consumer receives the message.
+     */
+    @Test
+    public void receiveMessage_messageSentBeforeConsumer_returnsOkAndMessage() {
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+        ConsumerId consumerId = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel("hello world"));
+    }
+
+    /**
+     * When a message is sent after a consumer is created, the consumer receives the message.
+     */
+    @Test
+    public void receiveMessage_messageSentAfterConsumer_returnsOkAndMessage() {
+        ConsumerId consumerId = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel("hello world"));
+    }
+
+    /**
+     * A message is only received once by a consumer.
+     */
+    @Test
+    public void receiveMessage_receivesMessageOnce() {
+        ConsumerId consumerId = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+
+        // Receives message on first call.
+        ResponseEntity<MessageModel> response = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualToComparingFieldByField(new MessageModel("hello world"));
+
+        // Receives empty on the second call.
+        ResponseEntity<MessageModel> response2 = restTemplate.postForEntity("/queue/receive/" + consumerId.getId(), null, MessageModel.class);
+        assertThat(response2.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response2.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * Only one consumer receives the same message.
+     */
+    @Test
+    public void receiveMessage_multipleConsumers_onlyOneReceivesMessage() {
+        ConsumerId consumerId1 = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+        ConsumerId consumerId2 = restTemplate.postForEntity("/queue/consumer", null, ConsumerId.class).getBody();
+
+        restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+
+        // Consumer 1 receives message.
+        ResponseEntity<MessageModel> response1 = restTemplate.postForEntity("/queue/receive/" + consumerId1.getId(), null, MessageModel.class);
+        assertThat(response1.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response1.getBody()).isEqualToComparingFieldByField(new MessageModel("hello world"));
+
+        // Consumer2 receives an empty message.
+        ResponseEntity<MessageModel> response2 = restTemplate.postForEntity("/queue/receive/" + consumerId2.getId(), null, MessageModel.class);
+        assertThat(response2.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response2.getBody()).isEqualToComparingFieldByField(new MessageModel(null));
+    }
+
+    /**
+     * Sending a message returns OK and an empty body.
+     */
+    @Test
+    public void sendMessage_returnsOkAndEmptyBody() {
+        ResponseEntity<Void> response = restTemplate.postForEntity("/queue/send", new MessageModel("hello world"), Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNull();
+    }
+}
