89a00b931d66307b0509a1db45d618cf54bfcf8d
Joshua Kitchen <joshdkitchen@gmail.com>
2019-04-22 13:50:01 +0100

    copied TopicService to SingleTopicService and added createProducer method

---
+++ simple-jms-broker/src/main/java/kitchen/josh/simplejms/broker/SingleTopicService.java
@@ -0,0 +1,43 @@
+package kitchen.josh.simplejms.broker;
+
+import java.util.*;
+
+public class SingleTopicService {
+
+    private final Map<UUID, Queue<String>> consumerQueues;
+    private final Set<UUID> producers;
+
+    SingleTopicService() {
+        consumerQueues = new HashMap<>();
+        producers = new HashSet<>();
+    }
+
+    UUID createConsumer() {
+        UUID consumerId = UUID.randomUUID();
+        consumerQueues.put(consumerId, new LinkedList<>());
+        return consumerId;
+    }
+
+    UUID createProducer() {
+        UUID producerId = UUID.randomUUID();
+        producers.add(producerId);
+        return producerId;
+    }
+
+    void addMessage(String message) {
+        consumerQueues.values().forEach(queue -> queue.add(message));
+    }
+
+    Optional<String> readMessage(UUID consumerId) {
+        return Optional.ofNullable(consumerQueues.get(consumerId))
+                .map(Queue::poll);
+    }
+
+    Map<UUID, Queue<String>> getConsumerQueues() {
+        return consumerQueues;
+    }
+
+    Set<UUID> getProducers() {
+        return producers;
+    }
+}
---
+++ simple-jms-broker/src/test/java/kitchen/josh/simplejms/broker/SingleTopicServiceTest.java
@@ -0,0 +1,123 @@
+package kitchen.josh.simplejms.broker;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingleTopicServiceTest {
+
+    private static final String MESSAGE_1 = "hello world";
+    private static final String MESSAGE_2 = "HELLO WORLD";
+
+    private SingleTopicService topicService;
+
+    @Before
+    public void setUp() {
+        topicService = new SingleTopicService();
+    }
+
+    @Test
+    public void createConsumer_shouldCreateUUIDAndAddEntryWithEmptyQueue() {
+        UUID consumerId = topicService.createConsumer();
+
+        assertThat(consumerId).isNotNull();
+        assertThat(topicService.getConsumerQueues()).containsOnlyKeys(consumerId);
+        Queue<String> consumerQueue = topicService.getConsumerQueues().get(consumerId);
+        assertThat(consumerQueue).isEmpty();
+    }
+
+    @Test
+    public void createConsumer_createsUniqueUUIDs() {
+        Set<UUID> consumerIds = IntStream.range(0, 10)
+                .mapToObj(i -> topicService.createConsumer())
+                .collect(Collectors.toSet());
+
+        assertThat(consumerIds).hasSize(10);
+        assertThat(topicService.getConsumerQueues()).containsOnlyKeys(new ArrayList<>(consumerIds).toArray(new UUID[]{}));
+        assertThat(topicService.getConsumerQueues().values()).allMatch(Collection::isEmpty);
+    }
+
+    @Test
+    public void createProducer_shouldCreateUUIDAndAddEntry() {
+        UUID producerId = topicService.createProducer();
+
+        assertThat(producerId).isNotNull();
+        assertThat(topicService.getProducers()).containsOnly(producerId);
+    }
+
+    @Test
+    public void createProducer_createsUniqueUUIDs() {
+        Set<UUID> producerIds = IntStream.range(0, 10)
+                .mapToObj(i -> topicService.createProducer())
+                .collect(Collectors.toSet());
+
+        assertThat(producerIds).hasSize(10);
+        assertThat(topicService.getProducers()).containsOnly(new ArrayList<>(producerIds).toArray(new UUID[]{}));
+    }
+
+    @Test
+    public void addMessage_noConsumers_doesNothing() {
+        topicService.addMessage(MESSAGE_1);
+
+        assertThat(topicService.getConsumerQueues()).isEmpty();
+    }
+
+    @Test
+    public void addMessage_consumersExist_appendsMessageToQueues() {
+        topicService.createConsumer();
+        topicService.createConsumer();
+
+        topicService.addMessage(MESSAGE_1);
+        topicService.addMessage(MESSAGE_2);
+
+        assertThat(topicService.getConsumerQueues()).hasSize(2);
+        topicService.getConsumerQueues().values().forEach(queue -> {
+            assertThat(queue.poll()).isEqualTo(MESSAGE_1);
+            assertThat(queue.poll()).isEqualTo(MESSAGE_2);
+        });
+    }
+
+    @Test
+    public void readMessage_consumerDoesNotExist_returnsEmpty() {
+        Optional<String> message = topicService.readMessage(UUID.randomUUID());
+        assertThat(message).isEmpty();
+    }
+
+    @Test
+    public void readMessage_consumerExists_emptyQueue_returnsEmpty() {
+        UUID consumerId = topicService.createConsumer();
+        Optional<String> message = topicService.readMessage(consumerId);
+        assertThat(message).isEmpty();
+    }
+
+    @Test
+    public void readMessage_consumerExistsWithMessage_returnsAndRemovesMessage() {
+        UUID consumerId = topicService.createConsumer();
+        topicService.addMessage(MESSAGE_1);
+        topicService.addMessage(MESSAGE_2);
+
+        Optional<String> message = topicService.readMessage(consumerId);
+
+        assertThat(message).contains(MESSAGE_1);
+        assertThat(topicService.getConsumerQueues().get(consumerId)).containsOnly(MESSAGE_2);
+    }
+
+    @Test
+    public void readMessage_multipleConsumers_onlyRemovesThisConsumersMessage() {
+        UUID consumerId = topicService.createConsumer();
+        UUID otherId = topicService.createConsumer();
+
+        topicService.addMessage(MESSAGE_1);
+        topicService.addMessage(MESSAGE_2);
+
+        Optional<String> message = topicService.readMessage(consumerId);
+        assertThat(message).contains(MESSAGE_1);
+        assertThat(topicService.getConsumerQueues().get(consumerId)).containsOnly(MESSAGE_2);
+        assertThat(topicService.getConsumerQueues().get(otherId)).containsOnly(MESSAGE_1, MESSAGE_2);
+    }
+}
