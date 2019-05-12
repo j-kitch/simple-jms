41b106fb39e238f6a7d40ce6a8b446cc8e621cd4
Joshua Kitchen <joshdkitchen@gmail.com>
2019-05-12 21:24:30 +0100

    refactored PropertyModelDeserializer

--- simple-jms-common/src/main/java/kitchen/josh/simplejms/common/PropertyModelDeserializer.java
+++ simple-jms-common/src/main/java/kitchen/josh/simplejms/common/PropertyModelDeserializer.java
@@ -18,58 +18,92 @@ import static org.springframework.util.NumberUtils.convertNumberToTargetClass;
 public class PropertyModelDeserializer extends JsonDeserializer<PropertyModel> {
 
     @Override
-    public PropertyModel deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    public PropertyModel deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
         try {
-            ObjectNode node = p.readValueAsTree();
-            Map<String, JsonNode> properties = toMap(node.fields());
-            if (properties.size() != 3) {
-                throw new JsonParseException(p, "Expected 3 properties");
-            }
-
-            String name = Optional.ofNullable(properties.get("name")).filter(JsonNode::isTextual).map(JsonNode::asText).orElseThrow(() -> new JsonParseException(p, "Expected field 'name'"));
-            String type = Optional.ofNullable(properties.get("type")).filter(JsonNode::isTextual).map(JsonNode::asText).orElseThrow(() -> new JsonParseException(p, "Expected field 'type'"));
-            JsonNode valueNode = Optional.ofNullable(properties.get("value")).orElseThrow(() -> new JsonParseException(p, "Expected field 'value'"));
-            Object value = parseValue(valueNode, type).orElseThrow(() -> new JsonParseException(p, "Expected field 'type' to have a Java primitive value"));
-
+            Map<String, JsonNode> properties = parseProperties(parser);
+            String name = parseName(parser, properties);
+            String type = parseType(parser, properties);
+            Object value = parseValue(parser, properties, type);
             return new PropertyModel(name, type, value);
         } catch (IllegalArgumentException iae) {
-            throw new JsonParseException(p, iae.getMessage());
+            throw new JsonParseException(parser, iae.getMessage());
         }
     }
 
-    private static <K, V> Map<K, V> toMap(Iterator<Map.Entry<K, V>> iterator) {
-        Map<K, V> map = new HashMap<>();
-        while (iterator.hasNext()) {
-            Map.Entry<K, V> entry = iterator.next();
-            map.put(entry.getKey(), entry.getValue());
+    private static Map<String, JsonNode> parseProperties(JsonParser parser) throws IOException {
+        ObjectNode node = parser.readValueAsTree();
+        Map<String, JsonNode> properties = toMap(node.fields());
+        if (properties.size() != 3) {
+            throw new JsonParseException(parser, "Expected 3 properties");
         }
-        return map;
+        return properties;
     }
 
-    private static Optional<Object> parseValue(JsonNode valueNode, String type) {
+    private static String parseName(JsonParser parser, Map<String, JsonNode> properties) throws JsonParseException {
+        return (String) parsePropertyByNameAndType(parser, properties, "name", "String");
+    }
+
+    private static String parseType(JsonParser parser, Map<String, JsonNode> properties) throws JsonParseException {
+        return (String) parsePropertyByNameAndType(parser, properties, "type", "String");
+    }
+
+    private static Object parseValue(JsonParser parser, Map<String, JsonNode> properties, String type) throws JsonParseException {
+        return parsePropertyByNameAndType(parser, properties, "value", type);
+    }
+
+    private static Object parsePropertyByNameAndType(JsonParser parser, Map<String, JsonNode> properties, String name, String type) throws JsonParseException {
+        return parseValueWithType(properties.get(name), type)
+                .orElseThrow(() -> new JsonParseException(parser, "Expected field name'" + type + "'"));
+    }
+
+    private static Optional<?> parseValueWithType(JsonNode valueNode, String type) {
         switch (type) {
             case "Boolean":
-                return toBoolean(valueNode);
+                return parseBooleanValue(valueNode);
             case "Byte":
-                return toByte(valueNode);
+                return parseByteValue(valueNode);
             case "Short":
-                return toShort(valueNode);
+                return parseShortValue(valueNode);
             case "Integer":
-                return toInt(valueNode);
+                return parseIntValue(valueNode);
             case "Long":
-                return toLong(valueNode);
+                return parseLongValue(valueNode);
             case "Float":
-                return toFloat(valueNode);
+                return parseFloatValue(valueNode);
             case "Double":
-                return toDouble(valueNode);
+                return parseDoubleValue(valueNode);
             case "String":
-                return toString(valueNode);
+                return parseStringValue(valueNode);
             default:
                 return Optional.empty();
         }
     }
 
-    private static Optional<Object> toLong(JsonNode node) {
+    private static Optional<Boolean> parseBooleanValue(JsonNode node) {
+        return Optional.ofNullable(node)
+                .filter(JsonNode::isBoolean)
+                .map(JsonNode::asBoolean);
+    }
+
+    private static Optional<Byte> parseByteValue(JsonNode node) {
+        return Optional.ofNullable(node)
+                .filter(JsonNode::isInt)
+                .map(n -> convertNumberToTargetClass(n.asInt(), Byte.class));
+    }
+
+    private static Optional<Short> parseShortValue(JsonNode node) {
+        return Optional.ofNullable(node)
+                .filter(JsonNode::isInt)
+                .map(n -> convertNumberToTargetClass(n.asInt(), Short.class));
+    }
+
+    private static Optional<Integer> parseIntValue(JsonNode node) {
+        return Optional.ofNullable(node)
+                .filter(JsonNode::isInt)
+                .map(JsonNode::asInt);
+    }
+
+    private static Optional<Long> parseLongValue(JsonNode node) {
         if (node.isInt()) {
             return Optional.of((long) node.asInt());
         }
@@ -79,47 +113,32 @@ public class PropertyModelDeserializer extends JsonDeserializer<PropertyModel> {
         return Optional.empty();
     }
 
-    private static Optional<Object> toBoolean(JsonNode node) {
-        return Optional.ofNullable(node)
-                .filter(JsonNode::isBoolean)
-                .map(JsonNode::asBoolean);
-    }
-
-    private static Optional<Object> toByte(JsonNode node) {
-        return Optional.ofNullable(node)
-                .filter(JsonNode::isInt)
-                .map(n -> convertNumberToTargetClass(n.asInt(), Byte.class));
-    }
-
-    private static Optional<Object> toShort(JsonNode node) {
-        return Optional.ofNullable(node)
-                .filter(JsonNode::isInt)
-                .map(n -> convertNumberToTargetClass(n.asInt(), Short.class));
-    }
-
-    private static Optional<Object> toInt(JsonNode node) {
-        return Optional.ofNullable(node)
-                .filter(JsonNode::isInt)
-                .map(JsonNode::asInt);
-    }
-
-    private static Optional<Object> toFloat(JsonNode node) {
+    private static Optional<Float> parseFloatValue(JsonNode node) {
         return Optional.ofNullable(node)
                 .filter(JsonNode::isDouble)
                 .map(JsonNode::asDouble)
                 .map(PropertyModelDeserializer::doubleToFloat);
     }
 
-    private static Optional<Object> toDouble(JsonNode node) {
+    private static Optional<Double> parseDoubleValue(JsonNode node) {
         return Optional.ofNullable(node)
                 .filter(JsonNode::isDouble)
                 .map(JsonNode::asDouble);
     }
 
-    private static Optional<Object> toString(JsonNode node) {
+    private static Optional<String> parseStringValue(JsonNode node) {
         return Optional.ofNullable(node)
                 .filter(JsonNode::isTextual)
                 .map(JsonNode::asText);
+    }
+
+    private static <K, V> Map<K, V> toMap(Iterator<Map.Entry<K, V>> iterator) {
+        Map<K, V> map = new HashMap<>();
+        while (iterator.hasNext()) {
+            Map.Entry<K, V> entry = iterator.next();
+            map.put(entry.getKey(), entry.getValue());
+        }
+        return map;
     }
 
     private static float doubleToFloat(double d) {
